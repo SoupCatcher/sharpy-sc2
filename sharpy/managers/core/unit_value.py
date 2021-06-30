92757e653883a82d1ad2447982d7ffdf2fed045a
SoupCatcher <ulf.norell+soupcatcher@gmail.com>
2021-06-30 17:18:50 +0200

    Use `real_type` when looking up unit value by type

--- sharpy/managers/core/unit_value.py
+++ sharpy/managers/core/unit_value.py
@@ -631,7 +631,7 @@ class UnitValue(ManagerBase, IUnitValues):
         return self.power_by_type(unit.type_id, health_percentage)
 
     def power_by_type(self, type_id: UnitTypeId, health_percentage: float = 1) -> float:
-        unit_value = self.unit_data.get(type_id, None)
+        unit_value = self.unit_data.get(self.real_type(type_id), None)
         if unit_value is not None:
             return unit_value.combat_value * health_percentage
         return 1.0 * health_percentage
